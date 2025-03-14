b5bb112585801d46673fdcf521b7dcccbb5692f7
baranariban <alibaranariban.metu@gmail.com>
2025-03-14 13:35:35 +0300

    Update and rename Option1.py to Composite Selector.py

--- pages/Composite Selector.py
+++ pages/Composite Selector.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-# Prevent unauthorized access
 if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
     st.error("Unauthorized access. Please log in.")
     st.stop()
