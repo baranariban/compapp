f933ced3d882b8113dffb946f4745dcbb865b25e
baranariban <alibaranariban.metu@gmail.com>
2025-03-14 13:40:06 +0300

    Update Composite Selector.py

--- pages/Composite Selector.py
+++ pages/Composite Selector.py
@@ -4,5 +4,7 @@ if "authenticated" not in st.session_state or not st.session_state["authenticate
     st.error("Unauthorized access. Please log in.")
     st.stop()
 
-st.title("Option 1 Page")
-st.write("Content for Option 1")
+st.title("CompApp: Composite Application")
+st.markdown("### :red[by Ali Baran Arıban]")
+st.title("Composite Selector")
+st.write("Click on the properties you want in your composite/polymer. Then fill in the blank spaces with the maximum or the minimum limits of the parameters you desire. The application will provide you a list of composites which are suitable for your project's requirements. You will also have a chance to compare these composites with total grades out of 100.")
